5f920c62c17440a19d6f8a4adc05c0090c249b26
imfeng <hermitf@big-cause.com>
2019-04-09 20:03:36 +0800

    first commit

---
+++ components/Navs.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="main-nav">
+    <nuxt-link to="./" class="button--green">首頁</nuxt-link>
+    <nuxt-link to="./about" class="button--green">關於我們</nuxt-link>
+    <nuxt-link to="./news" class="button--green">最新消息</nuxt-link>
+    <nuxt-link to="./lotto" class="button--green">彩期號碼</nuxt-link>
+</div>
+
+</template>
+
+<style lang="scss">
+    #main-nav {
+        text-align: center;
+    }
+</style>
