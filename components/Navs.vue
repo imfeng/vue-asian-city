5cf2274bde5e123d4603ffad4d38a5a607610367
imfeng <hermitf@big-cause.com>
2019-04-09 20:09:51 +0800

    webpage docs

--- components/Navs.vue
+++ components/Navs.vue
@@ -1,9 +1,14 @@
 <template>
 <div id="main-nav">
-    <nuxt-link to="./" class="button--green">首頁</nuxt-link>
+   <!-- <nuxt-link to="./" class="button--green">首頁</nuxt-link>
     <nuxt-link to="./about" class="button--green">關於我們</nuxt-link>
     <nuxt-link to="./news" class="button--green">最新消息</nuxt-link>
     <nuxt-link to="./lotto" class="button--green">彩期號碼</nuxt-link>
+    -->
+    <a href="./" class="button--grey">首頁</a>
+    <a href="./about" class="button--grey">關於我們</a>
+    <a href="./news" class="button--grey">最新消息</a>
+    <a href="./lotto" class="button--grey">彩期號碼</a>
 </div>
 
 </template>
